be173a3609fdf23d47a0032fc53c318e7eb7f60d
bmanjuu <bettyofung@gmail.com>
2016-07-07 09:22:57 -0400

    done

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,5 +1,7 @@
 let name = "Audrey"
 name.uppercaseString
+//always check out autocomplete stuff! super helpful 
+//hit cmd + shift + 0 to get doc/API reference from xcode window 
 
 let uppercaseName = name.uppercaseString
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
